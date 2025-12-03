1c7bb72696300fa8c22eeacde6c2173b03fb9e1e
henriquediasjr <henriquediasjr@gmail.com>
2025-12-03 20:10:29 -0300

    first class about routing

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'hello';
+})->name('hello');
+
+Route::get('/oi', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still nothing here';
+});
