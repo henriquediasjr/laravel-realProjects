533a829050c46e1328855bc231ee3fcf7a4c2872
henriquediasjr <henriquediasjr@gmail.com>
2025-12-03 20:18:45 -0300

    learning blade

---
+++ task-list/resources/views/index.blade.php
@@ -0,0 +1,6 @@
+Hello Im a blade template!
+
+
+@isset($name)
+    <div>The name is {{ $name }} and the age is {{ $age }}</div>
+@endisset
--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index', [
+        'name' => 'Henrique',
+        'age' => 20
+    ]);
 });
 
 Route::get('/hello', function () {
